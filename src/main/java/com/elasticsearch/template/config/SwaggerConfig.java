f628ab4fdff6d0f64f75f0c6ace5ef6a0ebd480b
Cihat Topsakal <cihattpskl@gmail.com>
2020-11-18 21:03:47 +0300

    swagger update

--- src/main/java/com/elasticsearch/template/config/SwaggerConfig.java
+++ src/main/java/com/elasticsearch/template/config/SwaggerConfig.java
@@ -6,42 +6,29 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @EnableSwagger2
 @Configuration
 public class SwaggerConfig implements WebMvcConfigurer {
 
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry
-                .addResourceHandler("swagger-ui.html")
-                .addResourceLocations("classpath:/META-INF/resources/");
-
-        registry
-                .addResourceHandler("/webjars/**")
-                .addResourceLocations("classpath:/META-INF/resources/webjars/");
-    }
-
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.elasticsource.template"))
-                .paths(PathSelectors.regex("/"))
-                .build();
-    }
-
-    private ApiInfo getApiInfo() {
-        return new ApiInfoBuilder()
-                .title("Swagger API Doc")
-                .description("More description about the API")
-                .version("1.0.0")
+                .apis(RequestHandlerSelectors.basePackage("com.elasticsearch.template"))
+                .paths(PathSelectors.regex("/.*"))
                 .build();
     }
 }
