1d6ae330c0741e35e2098d684f245c1b317641fd
Cihat Topsakal <cihattpskl@gmail.com>
2020-11-18 21:07:11 +0300

    schema.sql update

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,10 +1,13 @@
 DROP TABLE IF EXISTS `mysql_order`;
 
 CREATE TABLE `mysql_order` (
-  `order_id` int NOT NULL AUTO_INCREMENT,
-  `order_payment` varchar(255) DEFAULT NULL,
-  `order_tender_type` int DEFAULT NULL,
-  `order_time` varchar(255) DEFAULT NULL,
-  `order_type` varchar(255) DEFAULT NULL,
+  `orderId` varchar(255) NOT NULL AUTO_INCREMENT,
+  `orderPayment` varchar(255) DEFAULT NULL,
+  `orderTime` varchar(255) DEFAULT NULL,
+  `orderedByName` varchar(255) DEFAULT NULL,
+  `orderedBySurname` varchar(255) DEFAULT NULL,
+  `orderedByAddress` varchar(255) DEFAULT NULL,
+  `orderedByPhoneNumber` varchar(255) DEFAULT NULL,
+  `orderedByEmail` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`order_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4
