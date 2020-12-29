f34c2298dcb5a8892eb53226fc609daa1d3474bc
Cihat Topsakal <cihattpskl@gmail.com>
2020-12-29 15:39:03 +0300

    Fix swagger uı

--- src/main/java/com/elasticsearch/template/config/PopulatorConf.java
+++ src/main/java/com/elasticsearch/template/config/PopulatorConf.java
@@ -9,7 +9,6 @@ import org.springframework.data.repository.init.Jackson2RepositoryPopulatorFacto
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
-@EnableWebMvc
 public class PopulatorConf {
 
     @Value("${Jackson2RepositoryPopulatorJsonData}")
--- src/main/java/com/elasticsearch/template/config/SwaggerConf.java
+++ src/main/java/com/elasticsearch/template/config/SwaggerConf.java
@@ -2,32 +2,38 @@ package com.elasticsearch.template.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Parameter;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import java.util.ArrayList;
-import java.util.List;
-
-@EnableSwagger2
 @Configuration
-public class SwaggerConf implements WebMvcConfigurer {
+@EnableSwagger2
+public class SwaggerConf {
     @Bean
-    public Docket api() {
+    public Docket swaggerConfig(){
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.elasticsearch.template"))
-                .paths(PathSelectors.regex("/.*"))
+                .paths(PathSelectors.any())
+                .apis(RequestHandlerSelectors.any())
+                .build()
+                .apiInfo(metaData());
+    }
+
+    private void apiInfo(ApiInfo metaData) { }
+
+    private ApiInfo metaData(){
+        return new ApiInfoBuilder()
+                .title("Elasticsearch Rest Api Documentation ")
+                .description("\"Spring Boot Rest Api for ELK - Mysql Service\"")
+                .version("1.0.0")
+                .license("Apache License Version 2.0")
+                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0")
+                .contact(new Contact("Cihat Topsakal", "https://cihattpskl.medium.com/", "cihattpskl@gmail.com"))
                 .build();
     }
 }
